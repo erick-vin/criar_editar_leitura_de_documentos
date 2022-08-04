4c83738f182e7b305c89c13ad4ec338536810944
erick.santos3 <erick.santos@segfy.com>
2022-08-04 16:42:47 -0300

    Criando blocos para edicao do json

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -60,17 +60,17 @@
             <div class="d-flex justify-content-between">
               <label for="exampleFormControlTextarea1" class="form-label">  Json para leitura do Documento</label>
               <div>
-                <button @click="testarJson()" class="btn btn-secondary mb-3 justify-content-end mr-5">+ Bloco</button>
+                <button @click="adicionarBlocoJson()" class="btn btn-secondary mb-3 justify-content-end mr-5">+ Bloco</button>
                 &nbsp;
                 <button @click="testarJson()" class="btn btn-success mb-3 justify-content-end mr-5">Testar</button>
               </div>
             </div>
             <div class="form-check form-switch">
-              <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" v-model="modoDesenvolvedor">
+              <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" v-model="modoDesenvolvedor" @click="converterLeituraJson()">
               <label class="form-check-label" for="flexSwitchCheckDefault">Modo desenvolvedor</label>
             </div>
             <div v-if="modoDesenvolvedor">
-              <textarea class="form-control" id="exampleFormControlTextarea1" rows="30" v-model="converterLeituraJsonParaString" value="teset">
+              <textarea class="form-control" rows="20" v-model="blocosParaLeituraString">
                   
               </textarea>
             </div>
@@ -82,11 +82,21 @@
                 
                 <h5>Bloco {{index + 1}}</h5>
                 <!-- Detalhes começo -->
-                <p>
-                  <button class="btn btn-primary" type="button" data-bs-toggle="collapse" :data-bs-target="'#collapseDetalhes' + index" aria-expanded="false" aria-controls="collapseExample">
+                <div class="btn-group" role="group" aria-label="Basic outlined example">
+                  <button class="btn btn-outline-primary" type="button" data-bs-toggle="collapse" :data-bs-target="'#collapseDetalhes' + index" aria-expanded="false" aria-controls="collapseExample">
                     Detalhes
                   </button>
-                </p>
+                  <button class="btn btn-outline-primary" type="button" data-bs-toggle="collapse" :data-bs-target="'#collapseOptions' + index" aria-expanded="false" aria-controls="collapseExample">
+                    Opções
+                  </button>
+                  <button class="btn btn-outline-primary" type="button" data-bs-toggle="collapse" :data-bs-target="'#collapseFields' + index" aria-expanded="false" aria-controls="collapseExample">
+                    Fields
+                  </button>
+                  <button class="btn btn-outline-primary" type="button" data-bs-toggle="collapse" :data-bs-target="'#collapseCheckList' + index" aria-expanded="false" aria-controls="collapseExample">
+                    CheckList
+                  </button>
+                </div>
+                
                 <div class="collapse" :id="'collapseDetalhes' + index">
                   <div class="card card-body">
                     <div class="mb-4 row">
@@ -133,8 +143,94 @@
                   </div>
                 </div>
                 <!-- Detalhes Fim -->
+
+                <!-- Opções Começo -->
+                <div class="collapse" :id="'collapseOptions' + index">
+                  <div class="card card-body">
+                    <div class="mb-4 row">
+                      
+                      <div class="input-group">
+                        <div class="col-sm-8 mb-2">
+                          <select class="form-select" v-model="leituraOpcao">
+                            <option selected value="fix_upper_margin">fix_upper_margin</option>
+                            <option value="fix_lower_margin">fix_lower_margin</option>
+                            <option value="line_text">line_text</option>
+                            <option value="text">text</option>
+                          </select>
+                        </div>
+                        <div class="col-sm-4 mb-2">
+                          <button @click="adicionarOpcaoNaLeitura(index)" class="btn btn-success mb-3">Adicionar</button>
+                        </div>
+                      </div>
+
+                      <div v-for="(itemOpcao, indexOpcao) in item.options" :key="indexOpcao" class="row mb-2">
+                        <label class="col-sm-2 col-form-label">{{indexOpcao}}</label>
+                        <div class="col-sm-10">
+                          <select class="form-select" v-model="item.options[indexOpcao]">
+                            <option selected value="True">Sim</option>
+                            <option value="False">Não</option>
+                          </select>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!-- Opções Fim -->
+                
+                <!-- Fields Começo -->
+                <div class="collapse" :id="'collapseFields' + index">
+                  <div class="card card-body">
+                    <div class="mb-4 row">
+                      <h5 class="mb-4">
+                          Fields
+                        </h5>
+                      <div v-for="(field, indexField) in item.fields" :key="indexField">
+                        
+                        <!-- Input pagina começo -->
+                        <div class="row mt-2">
+                          <div class="col-sm-3 mb-2" v-for="(dado, indexDado) in field" :key="indexDado">
+                            <input type="text" class="form-control" v-model="field[indexDado]" @value="field[indexDado]">
+                            
+                          </div>
+                          <button @click="removerBlocoFields(indexField, index)" class="btn btn-danger mt-3">delete</button>
+                          <hr class="mt-3">
+                        </div>
+                        <!-- Input pagina Fim -->
+                        
+                      </div>
+                      <button @click="adicionarBlocoFields(index)" class="btn btn-secondary mt-3  mr-5">+ Bloco</button>
+                    </div>
+                  </div>
+                </div>
+                <!-- Fields Fim -->
+
+                <!-- CheckList Começo -->
+                <div class="collapse" :id="'collapseCheckList' + index">
+                  <div class="card card-body">
+                    <div class="mb-4 row">
+                      <h5 class="mb-4">
+                          CheckList
+                        </h5>
+                      <div v-for="(checklist, indexChecklist) in item.checklist" :key="indexChecklist">
+                        
+                        <!-- Input pagina começo -->
+                        <div class="row mt-2">
+                          <div class="col-sm-3 mb-2" v-for="(dado, indexDado) in checklist" :key="indexDado">
+                            <input type="text" class="form-control" v-model="checklist[indexDado]" @value="checklist[indexDado]">
+                            
+                          </div>
+                          <button @click="removerBlocoChecklist(indexChecklist, index)" class="btn btn-danger mt-3">delete</button>
+                          <hr class="mt-3">
+                        </div>
+                        <!-- Input pagina Fim -->
+                        
+                      </div>
+                      <button @click="adicionarBlocoChecklist(index)" class="btn btn-secondary mt-3  mr-5">+ Bloco</button>
+                    </div>
+                  </div>
+                </div>
+                <!-- CheckList Fim -->
               </div>
-              
             </div>
             <!-- Leitura com interface Fim -->
           </div>
@@ -147,18 +243,41 @@
 export default {
   data() {
     return {
+      leituraOpcao : '',
+      blocosParaLeituraString : '',
       blocosParaLeituraJson : [
         {
           'search_pages' : '123',
           'table_style': 'stream',
           'table_areas' : [10, 10, 10, 10],
-          'on_invalid_field': 'ignore'
+          'on_invalid_field': 'ignore',
+          'options' : {
+            'fix_upper_margin': 'True',
+            'fix_lower_margin': 'True',
+            'line_text': 'True'
+          },
+          'fields' : [
+            ['emissao', 'None', '', ''],
+            ['emissao2', 'None', '', ''],
+          ],
+          'checklist' : [
+            ['', '']
+          ]
         },
         {
           'search_pages' : '456',
-          'table_style': 'stream',
+          'table_style': 'lattice',
           'table_areas' : [10, 10, 10, 10],
-          'on_invalid_field': 'ignore'
+          'on_invalid_field': 'ignore',
+          'options' : {
+            'fix_upper_margin': 'True',
+            'fix_lower_margin': 'True',
+            'line_text': 'True'
+          },
+          'fields' : [],
+          'checklist' : [
+            
+          ]
         },
       ],
       dadosParaEnviar : [],
@@ -167,7 +286,6 @@ export default {
       inputPrefixo: '',
       inputValor: '',
       inputPagina: '1',
-      restultadoLeituraTeste : '',
       modoDesenvolvedor : false
     }
   },
@@ -175,11 +293,17 @@ export default {
     
   },
   computed: {
-    converterLeituraJsonParaString(){
-      return JSON.stringify(this.blocosParaLeituraJson)
-    },
+    
   },
   methods: {
+    converterLeituraJson(){
+      if(this.modoDesenvolvedor == true){
+        this.blocosParaLeituraJson = JSON.parse(this.blocosParaLeituraString)
+        console.log(this.blocosParaLeituraJson)
+      }else {
+        this.blocosParaLeituraString = JSON.stringify(this.blocosParaLeituraJson, null, 4)
+      }
+    },
     adicionarCampo(){
 
       this.dadosParaEnviar.push(
@@ -217,10 +341,51 @@ export default {
     },
 
     adicionarBlocoJson(){
+      this.blocosParaLeituraJson.push({
+        'search_pages': '1',
+        'table_style': 'stream',
+        'table_areas': [
+            0,
+            0,
+            0,
+            0
+        ],
+        'on_invalid_field': 'ignore',
+        'options': {
+            'fix_upper_margin': 'True',
+            'fix_lower_margin': 'True',
+            'line_text': 'True'
+        },
+        'fields': [
+            [
+              'campo', '', '', ''
+            ]
+        ],
+        'checklist': []
+    })
+    },
 
+    adicionarBlocoFields(index){
+      this.blocosParaLeituraJson[index].fields.push([
+        '', '', '', ''
+      ])
+    },
+    removerBlocoFields(indexField, index){
+      this.blocosParaLeituraJson[index].fields.splice(indexField, 1)
+    },
+    adicionarBlocoChecklist(index){
+      this.blocosParaLeituraJson[index].checklist.push([
+        '', ''
+      ])
+    },
+    removerBlocoChecklist(indexChecklist, index){
+      this.blocosParaLeituraJson[index].checklist.splice(indexChecklist, 1)
+    },
+
+    adicionarOpcaoNaLeitura(index){
+      this.blocosParaLeituraJson[index].options[this.leituraOpcao] = "True"
     }
-  },
-    
+  }
 }
 </script>
 
